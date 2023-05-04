c15476be37ec1a27ce9c695fdd494f89b7a58c46
ashanuyangoda <ashanuyangoda@gmail.com>
2023-05-04 21:06:23 +0530

    Add link to setup.py file. Version change

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
 setuptools.setup(
 name='wav2samp',
-version='0.1',
+version='0.2',
 author="Ashan Uyangoda",
 author_email="ashan7git@gmail.com",
 description="Convert wav file in to text file with audio samples and vice versa",
 long_description="This package is mainly use for DSP purposes. This package contains different functions to convert wav files in to text files with audio samples and convert text files with audio samples in to wav files. This package supports both Stereo and Mono.",
+url="https://github.com/ashanuyangoda/wav2samp",
 packages=setuptools.find_packages(),
 install_requires=['numpy', 'scipy'],
 classifiers=[
